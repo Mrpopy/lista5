4b99c88eea3ced231c81d966fdc8fa8b4d8f2e8a
Tiago Félix <143558157+Mrpopy@users.noreply.github.com>
2024-09-20 16:33:10 -0300

    Delete q6 directory

--- q6/src/Smartphone.java
+++
@@ -1,6 +0,0 @@
-public class Smartphone extends Eletronico {
-
-    public Smartphone(String marca, String modelo, String preco, String descricao) {
-        super(marca, modelo, preco, descricao);
-    }
-}
